359e154358147a76c84af6120a5a20a132e50950
Holly <36353656+theparadoxxdev@users.noreply.github.com>
2024-04-20 13:23:58 -0700

    Created VS Code WS and Rust project

---
+++ rust-test/target/debug/deps/rust_test-16a595094cfd8b55.d
@@ -0,0 +1,5 @@
+/Users/holly/Documents/GitHub/Rust-Test/rust-test/target/debug/deps/librust_test-16a595094cfd8b55.rmeta: src/main.rs
+
+/Users/holly/Documents/GitHub/Rust-Test/rust-test/target/debug/deps/rust_test-16a595094cfd8b55.d: src/main.rs
+
+src/main.rs:
---
+++ rust-test/target/debug/deps/rust_test-857ec6870af4e94d.d
@@ -0,0 +1,5 @@
+/Users/holly/Documents/GitHub/Rust-Test/rust-test/target/debug/deps/librust_test-857ec6870af4e94d.rmeta: src/main.rs
+
+/Users/holly/Documents/GitHub/Rust-Test/rust-test/target/debug/deps/rust_test-857ec6870af4e94d.d: src/main.rs
+
+src/main.rs:
